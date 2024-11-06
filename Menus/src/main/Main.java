92ca6dee795557ef9ab725c020889bff65e0e2b6
tPdru <pedro.andrade64@etec.sp.gov.br>
2024-11-05 21:47:12 -0300

    Splash scream e tela de login prontas

---
+++ Menus/src/main/Main.java
@@ -0,0 +1,14 @@
+
+package main;
+
+import View.menu_GUI;
+import View.splash_GUI;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+         new splash_GUI().setVisible(true);
+    }
+    
+}
